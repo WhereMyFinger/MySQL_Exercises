c78054d484336afe0cc12846c71f60a481477046
NguyenLe241299 <49112036+WhereMyFinger@users.noreply.github.com>
2022-06-21 10:59:24 +0700

    upload exercise06

---
+++ exercise06.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    first_name VARCHAR(45),
+    second_name VARCHAR(45),
+    attribute VARCHAR(200)
+);
+
+insert into users
+value
+('Mikel', 'Cover', '%Mikel_Cover%'),
+('Vicenta', 'Kravitz', '%Vicenta_Kravitz'),
+('Tosha', 'Cover', 'SALKF91204NSDAW2kdfh8SFDSKv9293'),
+('Shayne', 'Dahlquist', '0JSDJSK8WkihjD8IK%Shayne_Dahlquist%JHSJjh85'),
+('Lizabeth', 'Cover', 'asdnjan9Jh8INKJh8inkNIN9989sj'),
+('Mikel', 'Kravitz', 'PAESH82kjncdsj88%Mike_Kravitz%mskdnKKjdks9832'),
+('Josphine', 'Arzate', 'kdfjkHJHJS82JUjjkdh%Josphine_arzate%sjhfd82JJDNKJ8'),
+('Deidre', 'Chace', 'nsjbJBJBCJD98Njjjnsi');
+
+
+
+insert into users
+value
+('nick', 'cage', 'HSHUSJsj2k24k%nickkcage%kmkc9KKM9jf'),
+('uncle', 'ben', 'kknjJNJJNJj82nuncle_benkmskmksKJMKSk8');
+
+select * from users
+where binary attribute like concat('_%!%', first_name, '!_', second_name,char(37), '!%%') escape '!';
